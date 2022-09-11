3a6114e3a00adb4cc51c07bc3b31c617e8fcd3cf
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-10 23:30:22 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
