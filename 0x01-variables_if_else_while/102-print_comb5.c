67b398eb6904bf1ec3facb4fe2db25183a5bca40
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-11 07:54:00 -0700

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+	int second;
+
+	for (digit = 0; digit <= 98; digit++)
+	{
+		for (second = digit + 1; second < 99; second++)
+		{
+			putchar((digit % 10) + '0');
+			putchar((digit % 10) + '0');
+			putchar(' ');
+			putchar((second % 10) + '0');
+			putchar((second % 10) + '0');
+
+			if (digit ==98 && second == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
