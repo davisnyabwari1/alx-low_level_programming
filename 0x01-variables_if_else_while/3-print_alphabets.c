7059a04e086385fa4881c33b48d6af0b22b0a924
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-10 22:31:49 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+	char alphabet = 'A';
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (alphabet ='A'; alphabet <= 'Z'; alphabet++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
