2089c08240f4ce3021f2338f4d6080439ff18dab
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-10 23:20:04 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char digit = '0';
+	char letter = 'a';
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+	for (letter = 'a'; letter <= 'f' letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
