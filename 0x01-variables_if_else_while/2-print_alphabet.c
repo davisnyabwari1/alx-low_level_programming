ae704d32eac4ade043dd3aed96d928320dbbdc06
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-10 22:14:39 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -14,9 +14,10 @@ int main(void)
 {
 	char letter = 'a';
 
-	for (letter = 'a'; letter <= 'z'; letter ++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
+		putchar("\n");
 	}
 	return (0);
 }
