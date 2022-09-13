eb8dfcfe73144f35e7d5db4db1cabc140f1ca0f2
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-13 12:10:49 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 #include "main.h"
 
+
+
+
 /**
-*
-*
-*
 * prints alphabets from a-z
 *
 */
 void print_alphabet(void)
-{	
+{
 	char alphabet = 'a';
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	{	
+	{
 		_putchar(alphabet);
 	}
 	_putchar('\n');
