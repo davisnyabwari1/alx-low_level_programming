d21a52f0c94e4428a5431720066f783874876ceb
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-14 02:49:13 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+*_abs - determines the abs value of a number
+*
+*
+*Return:Always 0(success)
+*/
+int _abs(int)
+{
+	int number,absnum;
+	absnum = abs (number);
+	return (0);
+}
