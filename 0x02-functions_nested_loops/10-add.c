c4d7e8de9fe70754dfa3896972c552b4291187bb
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-14 07:12:54 -0700

    Final task solution

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+*add - adds two values
+*@num1: value to be used
+*@num2: value to be used
+*Return:Always 0(success)
+*/
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
