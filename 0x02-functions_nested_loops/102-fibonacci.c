1c0ece2ba9e8280f74d7ad848275f5c9a0c7b7e4
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-14 14:47:30 -0700

    Final task solution

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+*main - Entry point
+*
+*Return:Always 0(success)
+*/
+int main(void)
+{
+	int t1 = 1, t2 = 2, nextTerm = 0;
+	nextTerm = t1 + t2;
+	printf("%d,%d", t1, t2);
+	
+	while (nextTerm <= 50)
+	{
+		printf("%d", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+	
+
+}
