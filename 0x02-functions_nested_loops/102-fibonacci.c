e154d9e2e5c21d01100368ed7c77a4bea169c107
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-14 15:19:03 -0700

    Final task solution

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 	int count;
 	unsigned long t1 = 0, t2 = 1, sum;
 
-	for (count = 0; count < 50; count ++)
+	for (count = 0; count < 50; count++)
 	{
 		sum = t1 + t2;
 		printf("%lu", sum);
@@ -21,7 +21,7 @@ int main(void)
 		if (count == 49)
 			printf("\n");
 		else
-			printf(",");
+			printf(", ");
 	}
 	return (0);
 }
