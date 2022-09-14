f7ede0260dd07024fed9cc80dc82cfc87126d972
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-14 08:14:13 -0700

    Final task solution

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,7 @@ void print_to_98(int n)
 			if (i > 98)
 				printf("%d, ", i);
 
-			printf("%d\n", i);
+			printf("%d", i);
 		}
 	}
 	else
@@ -33,5 +33,4 @@ void print_to_98(int n)
 		}
 
 	}
-	_putchar('\n');
 }
