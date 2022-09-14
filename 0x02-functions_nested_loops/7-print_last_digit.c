3ffa4ffbc9bfe2525cef5428a829a83806962c64
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-14 03:45:42 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include "main.h"
-
+#include<stdlib.h>
 
 /**
 *print_last_digit - determines the last digit of a number
@@ -11,10 +11,15 @@
 int print_last_digit(int number)
 {
 	int lastdigit;
-
-	if (lastdigit < 0)
-		return (abs(number) %  10);
+	
+	if (number < 0)
+	{
+		lastdigit = abs(number) % 10;
+		return (lastdigit);
+	}
 	else
+	{
 		lastdigit = number % 10;
 		return (lastdigit);
+	}
 }
