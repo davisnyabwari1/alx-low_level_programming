f2c7112b2619db6db3b0a97eb6e0b52db01e19f2
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-14 02:12:16 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+
+/**
+*print_sign - determines whether a number is +, negative or zero
+*@n: character checked
+*
+*Return:Always 0(success)
+*/
+int print_sign(int n)
+{
+	if ( n > 0)
+		return (1);
+	else if ( n == 0)
+		return (0);
+	else if ( n < 0)
+		return (-1);
+}
