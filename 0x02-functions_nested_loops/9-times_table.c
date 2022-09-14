0fbbfcc3fcc438e8773087c66ab4a6bdb782bc8d
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-14 06:13:52 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+
+
+
+
+/**
+*times_table- prints time from 0 to 23
+*/
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			printf("%d, ", i*j);
+		}
+		_putchar('\n');
+	}
+}
