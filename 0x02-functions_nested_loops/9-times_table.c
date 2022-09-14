31aba4b497d0c2a62545b831afa0e7e92b9baf7d
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-14 06:50:49 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,15 +9,26 @@
 */
 void times_table(void)
 {
-	int i, j;
+	int i, j, prdct;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+
+		for (j = 1; j <= 9; j++)
 		{
-			printf("%d, ", i * j);
+			_putchar(',');
+			_putchar(' ');
+
+			prdct = i * j;
+
+			if (prdct <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
+		_putchar('\n');
 	
-		printf('\n');
 	}
 }
