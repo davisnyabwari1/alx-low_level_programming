4f9cc4d606919c52cdf4315cb4899bc70307604f
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-14 06:52:49 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,8 +25,8 @@ void times_table(void)
 			if (prdct <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+				_putchar((prdct / 10) + '0');
+			_putchar((prdct % 10) + '0');
 		}
 		_putchar('\n');
 	
