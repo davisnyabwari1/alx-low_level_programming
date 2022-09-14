f1ba72e5e6754e0ae3bdce056fcf03ca4877ac67
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-14 13:38:45 -0700

    Final task solution

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,12 +29,12 @@ void print_times_table(int n)
 					_putchar(' ');
 				if (prdct <= 9)
 					_putchar(' ');
-				if (prod >= 100)
+				if (prdct >= 100)
 				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
+					_putchar((prdct / 100) + '0');
+					_putchar(((prdct / 10)) % 10 + '0');
 				}
-				else if (prod <= 99 && prod >= 10)
+				else if (prdct <= 99 && prdct >= 10)
 				{
 					_putchar((prdct / 10) + '0');
 				}
