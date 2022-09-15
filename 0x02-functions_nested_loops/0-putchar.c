e6a23cc09f7bc2bc19f0b7cf9288b7ea6c06644c
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-15 10:52:29 -0700

    Final task solution

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,9 +9,15 @@
 */
 int main(void)
 {
-	char letter;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	if (_islower(letter) > 0)
-		return (1);
 	return (0);
 }
