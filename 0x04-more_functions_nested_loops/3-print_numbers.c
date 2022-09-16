7adf98054224cf269d4a3013d0a2d7cb7ec766b4
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-16 00:46:09 -0700

    Final task solution

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "main.h"
+
+
+
+
+/**
+*print_numbers- prints alphabets from a-z ten times
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+
+	}
+	_putchar('\n');
+}
