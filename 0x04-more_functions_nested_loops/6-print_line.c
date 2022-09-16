1206ccbc61d220d5a446bc1c525977874ed7b12c
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-16 07:09:41 -0700

    Final task solution

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a line on the terminal
+ * @n: number of times an undescore is printed
+ */
+void print_line(int n)
+{
+	int counter;
+
+	if (n > 0)
+	{
+		counter  = 0;
+		while (counter <= n)
+		{
+		       _putchar('_');
+		       counter++;
+		}
+		_putchar('\n');
+	}
+
+}
+			
