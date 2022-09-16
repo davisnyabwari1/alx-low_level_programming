474c33318a7ad1d1c2c2f63fc9170299929126b0
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-16 01:12:23 -0700

    Final task solution

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+* print_most_numbers- prints numbers from 0-9 except 2 & 4
+* 
+*/
+void print_most_umbers(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+if (i == 2 || i == 4)
+{
+continue;
+}
+else
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
