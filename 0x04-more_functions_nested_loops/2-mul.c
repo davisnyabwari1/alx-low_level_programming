e63b98abad173f4d7ac088234e75580807629be2
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-16 00:36:15 -0700

    Final task solution

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+*mul - adds two values
+*@num1: value to be used
+*@num2: value to be used
+*Return:Always 0(success)
+*/
+int mul(int num1, int num2)
+{
+	int result;
+
+	result = num1 * num2;
+	return (result);
+}
