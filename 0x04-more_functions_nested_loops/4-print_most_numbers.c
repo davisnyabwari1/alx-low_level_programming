4827bceb8ab9766ea09c603b1eee8a3dceba8334
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-16 01:16:36 -0700

    Final task solution

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,17 +7,14 @@
 */
 void print_most_umbers(void)
 {
-int i;
+	int i;
 
-for (i = 0; i <= 9; i++)
-{
-if (i == 2 || i == 4)
-{
-continue;
-}
-else
-{
-_putchar(i + '0');
-}
-_putchar('\n');
+	for (i = 0; i <= 9; i++)
+	{	
+		if (i == 2 || i == 4)
+			continue;
+		else
+			_putchar(i + '0');
+	}
+	_putchar('\n');
 }
