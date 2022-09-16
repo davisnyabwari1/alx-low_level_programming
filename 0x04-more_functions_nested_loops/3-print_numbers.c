ec1391d94f4364cfa4389bc80c257d8c7a7f1b31
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-16 01:00:26 -0700

    Final task solution

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,15 @@
 #include "main.h"
 
 /**
-*print_numbers- prints alphabets from a-z ten times
+* print_numbers- prints numbers from 0-9
 */
 void print_numbers(void)
 {
-	int i;
-	
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
