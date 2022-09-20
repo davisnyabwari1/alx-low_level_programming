3fa5634daf3242835a83d233bb4cdad16f92c8f0
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-20 15:49:37 -0700

    Final task solution

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+*_atoi - converts a string to an integet 
+*@s: string to be passed
+*Return: success
+*/
+int _atoi(char *s)
+{
+ int val;
+ val = atoi(s);
+return (0);
+}
