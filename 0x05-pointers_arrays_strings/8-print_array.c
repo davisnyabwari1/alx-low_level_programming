24ad1d2b911b486a07fad92a382084dca0322fde
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-20 15:11:13 -0700

    Final task solution

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*print_array - prints an arraay
+*@a -array 
+*n: number of elements
+*Return: success
+*/
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i == n -1)
+{
+continue;
+}
+else 
+{
+printf(", ");
+}
+}
+_putchar('\n');
+}
