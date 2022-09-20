cc7467b445f8e1bfa5c5442cfe238936550dcee4
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-20 14:07:59 -0700

    Final task solution

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*puts2 - finds length of a string
+*@s:string passed
+*Return: success
+*/
+void puts2(char *s)
+{
+int length, i;
+char ch;
+length = strlen(s);
+
+for (i = 0; i <= length - 1; i--)
+{
+ch = s[i];
+_putchar(ch);
+}
+_putchar('\n');
+}
