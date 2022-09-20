bfc3389d6a4915aa8dd2b7ae73ea91e397559d5a
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-20 15:25:49 -0700

    Final task solution

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+**_strcpy - copies a string
+*@dest:string passed
+*@str: src string
+*Return: success
+*/
+char *_strcpy(char *dest, char *src);
+{
+strcpy(dest, src);
+return(dest);
+}
