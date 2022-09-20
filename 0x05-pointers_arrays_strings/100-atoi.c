62038581c7ce485ecb7378c60ce20b8d3543d1d8
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-20 16:12:46 -0700

    Final task solution

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,15 +1,36 @@
 #include "main.h"
-#include <string.h>
 #include <stdlib.h>
-
 /**
-*_atoi - converts a string to an integet
-*@s: string to be passed
-*Return: success
-*/
+ * _atoi - Convert a string to integer.
+ * @s: char array string
+ * Return: first integer found in string
+ */
+
 int _atoi(char *s)
 {
-int val;
-val = atoi(s);
-return (val);
+	int i;
+	int h, p;
+
+	h = 0;
+	p = -1;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			p *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
+		{
+			if (h < 0)
+				h = (h * 10) - (s[i] - '0');
+			else
+				h = (s[i] - '0') * -1;
+
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
+	}
+	if (p < 0)
+		h *= -1;
+
+	return (h);
 }
