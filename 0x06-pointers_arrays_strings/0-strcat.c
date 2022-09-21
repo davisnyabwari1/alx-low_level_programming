70a28072e894b8119fbccaf92d8507d35eba493f
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-21 07:39:35 -0700

    Final task solution

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*
+*_strcat - concatenates two strings
+*@dest: destination string
+*@src: source string
+*Return: success
+*/
+
+char *_strcat(char *dest, char *src)
+{
+dest = strcat(dest,src);
+return (dest);
+}
+
