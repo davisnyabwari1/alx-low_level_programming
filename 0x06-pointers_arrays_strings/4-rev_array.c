3c2ad3eec088fefc0bd207e0b94ee52f8dbb31ac
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-22 04:58:13 -0700

    Final task solution

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,22 +1,19 @@
 #include "main.h"
-#include <string.h>
 
 /**
 *reverse_string - finds reverse of a string
 *@a:array passed
 *@n:number
-*Return: success
 */
 void reverse_array(int *a, int n);
 {
-int i;
-char ch;
+int i, ch;
 
 for (i = n - 1; i >= n / 2; i--)
 {
 ch = a[n - 1 - i];
 a[n - 1 - i] = a[i];
 a[i] = ch;
-
 }
+
 }
