25635ab41ea1c28413c23fc7116063ac6193df10
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-22 03:52:13 -0700

    Final task solution

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,19 +11,6 @@ void reverse_array(int *a, int n)
 {
 int i;
 
-for (i = 0; i < n; i++)
-{
-printf("%d", a[i]);
-if (i == n - 1)
-{
-continue;
-}
-else
-{
-printf(", ");
-}
-}
-printf("\n");
 for (i = n; i > 0; i--)
 {
 printf("%d", a[i]);
