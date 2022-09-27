fb9bcf6a6ba4ed3bc32c6742a02f69035f93d799
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-27 05:12:22 -0700

    Final task solution

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strpbrk - function that returns number of characters that match
+*@s: string to be scanned
+*@accept: string containing character to be matched
+*Return:  success
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	if (strpbrk(s,accept) != NULL)
+		return (*(strpbrk(s,accept)));
+	return (NULL);
+}
