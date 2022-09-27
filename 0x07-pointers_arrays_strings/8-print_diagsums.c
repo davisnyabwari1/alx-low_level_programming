2ac685ef153964a15261164bc2efd62ee918422c
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-27 15:08:06 -0700

    Final task solution

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,7 +7,7 @@
 void print_diagsums(int *a, int size)
 {
 	int i, sum1 = 0, sum2 = 0;
-	
+
 	for (i = 0; i < size; i++)
 	{
 		sum1 += a[i];
@@ -15,7 +15,7 @@ void print_diagsums(int *a, int size)
 	}
 	a -= size;
 
-	for (i = size; i >= 0; i--)
+	for (i = size; i >= 0; i++)
 	{
 		sum2 -= a[i];
 		a -= size;
