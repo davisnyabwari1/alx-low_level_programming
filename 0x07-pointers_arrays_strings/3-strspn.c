c02e05b5af7b79578d081768c10e6e5b1dc34c7b
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-27 04:42:23 -0700

    Final task solution

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strspn - function that returns number of characters that match
+*@s: string to be scanned
+*@accept: string containing character to be matched
+*Return:  success
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(*s, *accept));
+}
