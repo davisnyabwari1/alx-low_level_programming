ce547b7a950a8d80554ba8255d02c95e726dd887
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-29 08:27:56 -0700

    Final task solution

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_puts_recursion -prints a string using recursion
+*@s: string to be printed
+*/
+void _puts_recursion(char *s)
+{
+	if (*s != NULL)
+		puts(*s);
+		_puts_recursion(s + 1);
+	return;
+
+}
