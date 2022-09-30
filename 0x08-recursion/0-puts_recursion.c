4ce4719b95cc58ce9ebfe065f06daf3ff4a0aa23
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-09-29 21:50:08 -0700

    Final task solution

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,9 +5,10 @@
 */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		putchar('\n');
-	putchar(*s);
-	s++;
-	_puts_recursion(s);
+	if (*s != '\0')
+		putchar(*s);
+		s++;
+		_puts_recursion(s);
+	return;
+
 }
