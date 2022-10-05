f2ad16f99809dc3ae2c3b565d60294bbfdea74f6
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-10-05 02:25:43 -0700

    Final task solution

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*main - this the entry point of the function
+*@argc: argument count variable
+*@argv: string passed in contained here
+*Return: success
+*/
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc < 3)
+		printf("ERROR\n");
+	else
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+	}
+	return (1);
+}
