91a1950630db05e349af0e556cf53613d293880f
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-10-05 00:22:55 -0700

    Final task solution

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*main - this the entry point of the function
+*@argc: argument count variable
+*@argv: string passed in contained here
+*Return: success
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
