34dadb6c3e9b57768e16d12fd5b83929acb597b2
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-10-04 23:49:05 -0700

    Final task solution

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*main - this the entry point of the function
+*@argc: argument count variable
+*@argv: string passed in contained here
+*Return: success
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
