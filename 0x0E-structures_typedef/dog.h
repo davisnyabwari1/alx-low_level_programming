e336b7806eed8d900aa258aeb0a40e7ba7cba6f1
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-10-11 12:17:30 -0700

    Final

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*struct dog - dog properties
+*@name: dog's name
+*@age: dog's age
+*@owner: owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
