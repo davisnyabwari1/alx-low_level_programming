c3936d327d06a40b4f830da3f190a62387f64e25
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-10-12 09:00:04 -0700

    Final

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,9 +13,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d = malloc(sizeof(struct dog));
 
-	if (d == NULL)
-		printf("No Memory allocated");
-	else
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
