5c1a31abd8091f3cba3f1b3fa1ba57fbd46b8ced
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-10-25 06:11:19 -0700

    Final

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,19 +10,17 @@
 size_t print_list(const list_t *h)
 {
 	int count_node = 0;
-	
+
 	while (h)
 	{
-		
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 		{
-			printf("[%d] %s\n",h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		count_node++;
 	}
-	
 	return (count_node);
 }
