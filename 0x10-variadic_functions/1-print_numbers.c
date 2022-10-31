63b4b452e42ed011233ee1d9c3590f3b278cd8f8
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-10-31 07:15:16 -0700

    Final sln

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,13 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	for (i = 0; i < n; i++)
+	if (separator[0] != '\0')
 	{
-		if (separator[0] != '\0')
+		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(args, int));
-				if (i < n - 1)
-					printf("%s", separator);
+			if (i < n - 1)
+				printf("%s", separator);
 		}
 	}
 	va_end(args);
