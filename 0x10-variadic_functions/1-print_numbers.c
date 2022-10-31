4cc002fb4d870e54facbd0fa8f1dc58e8ba3322a
davisnyabwari1 <davisnyabwari1@gmail.com>
2022-10-30 22:34:30 -0700

    Final commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+*print_numbers - variadic function that prints numbers
+*@separator: separates values with coma
+*@n: numbers to be printed
+*Return: 0
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL)
+			printf("%d", va_arg(args, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
